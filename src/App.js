7f73e4342257655f6950de6e6c97841a70480858
Tejasvi Raj Pant <tejpant@gmail.com>
2021-04-07 13:40:23 +0545

    Created info header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Home from './Pages/home';
+import {Container} from 'react-bootstrap';
 
 function App() {
   return (
     <div style = {{backgroundColor: '#EDEDED', width: '100%', minHeight: '100%'}} >
+          <Container fluid>
           <Home/>
+
+          </Container>
     </div>
   );
 }
