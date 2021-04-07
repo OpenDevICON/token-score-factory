0b4229c897f355f60e2dfc32cf74b0ba87d7cdc0
Tejasvi Raj Pant <tejpant@gmail.com>
2021-04-07 11:59:58 +0545

    Created Charges block in Final Step Card

--- src/Components/home/form/FinalStepsCard/index.js
+++ src/Components/home/form/FinalStepsCard/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import AppInputField from '../AppInputField';
 import { networkMapping } from 'Constant';
-import { Col, Form } from 'react-bootstrap';
+import { Col, Form, Row, Button } from 'react-bootstrap';
 
 const FinalStepsCard = (formik) => {
 
     return (
-        <Card style={{ width: '100%', marginRight: '20px', height: '100%' }}>
+        <Card style={{ width: '100%', marginTop: '20px', marginBottom: '20px' }}>
             <Card.Body>
 
                 <div className="form-header" style={{ textAlign: 'center' }}>Final Steps</div>
 
                 <hr className="form-hr" />
-                <Col lg="6">
+                <Row>
+                <Col lg="6" style = {{marginTop: '75px'}}>
 
                     <AppInputField as="select" formik={formik} label="Network" id="network" placeholder="Name of your token" >
                         {/* <option value="" disabled selected = "selected">Choose a token type</option> */}
@@ -37,7 +38,36 @@ const FinalStepsCard = (formik) => {
                     </Form.Group>
                 </Col>
 
-                <button type="submit">Submit</button>
+                <Col lg = "6">
+                    <div className = "charge-header">
+                        <div>
+                            Charges
+                        </div>
+                    </div>
+
+                    <Row style = {{marginTop: '15px'}}>
+                        <Col xs = "6">
+                            <Form.Label>Commission Fee:</Form.Label>
+                        </Col>
+                        <Col xs = "6" style = {{display: 'flex', alignItems: 'center'}}>
+                            <span className = "charge-value">1 ICX</span>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xs = "6">
+                            <Form.Label>Transaction Fee:</Form.Label>
+                        </Col>
+                        <Col xs = "6" style = {{display: 'flex', alignItems: 'center'}}>
+                            <span className = "charge-value">0.00124 ICX</span>
+                        </Col>
+                    </Row>
+
+                </Col>
+                </Row>
+                <div className = "submit-button-container">
+                    <Button variant="info" type="submit">CREATE</Button>{' '}
+                </div>
             </Card.Body>
         </Card>
     )
