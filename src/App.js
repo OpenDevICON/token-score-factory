6368815356d71b06765ec6b00f2d30413cae6804
Tejasvi Raj Pant <tejpant@gmail.com>
2021-04-08 11:17:20 +0545

    Change Component name from Home to CreatePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Home from './Pages/home';
+import CreatePage from './Pages/CreatePage';
 import {Container} from 'react-bootstrap';
 
 function App() {
   return (
     <div style = {{backgroundColor: '#EDEDED', width: '100%', minHeight: '100%'}} >
           <Container fluid>
-          <Home/>
+          <CreatePage/>
 
           </Container>
     </div>
---
+++ src/Pages/CreatePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import InputForm from '../../Components/CreatePage/Form';
+import Header from '../../Components/CreatePage/Header';
+
+const CreatePage = () => {
+    return( 
+        <>
+            <Header />
+            <InputForm />
+        </>
+    )
+}
+
+export default CreatePage;
