343ac42f67d87464851eab8515fa0ab4cda9d35d
Tejasvi Raj Pant <tejpant@gmail.com>
2021-04-09 14:57:41 +0545

    Added button in the homepage to navigate to create page

--- Frontend/src/Components/HomePage/HomePageHeader/index.js
+++ Frontend/src/Components/HomePage/HomePageHeader/index.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap'
 import infoSvg from 'Assets/svg/info-img.svg';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const HomePageHeader = () => {
 
     return (
         <>
             <Row className="info-header">
-                <Col lg="4" className = "tsf-info-image-container">
-                    <img src = {infoSvg} alt = "tsf info" />
+                <Col lg="4" className="tsf-info-image-container">
+                    <img src={infoSvg} alt="tsf info" />
 
                 </Col>
 
-                <Col lg="8" className = "header-info-container">
-                    <div className = "info-title">
+                <Col lg="8" className="header-info-container">
+                    <div className="info-title">
                         CREATE YOUR OWN IRC TOKENS WITH EASE
                     </div>
 
-                    <div className = "info-desc-container">
+                    <div className="info-desc-container">
                         <svg width="23" height="99" viewBox="0 0 23 99" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <line x1="11.5" y1="-5.02681e-07" x2="11.5" y2="99" stroke="#B6D3D3" stroke-width="23" />
                         </svg>
 
-                        <span className = "info-desc">
-                            Easily deploy Smart Contract for a Standard, Capped, Mintable, Burnable, Payable IRC Token. No login. No setup. No coding required.
+                        <span className="info-desc">
+                            <span>Easily deploy Smart Contract for a Standard, Capped, Mintable, Burnable, Payable IRC Token. No login. No setup. No coding required.</span>
+                            <Link to="/create">
+                                <Button variant="primary">CREATE NOW</Button>
+                            </Link>
                         </span>
+
                     </div>
+
                 </Col>
             </Row>
         </>
