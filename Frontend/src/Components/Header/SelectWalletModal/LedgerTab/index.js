6a4675408a5b8f81cde2288e07615799b0e362a5
Tejasvi Raj Pant <tejpant@gmail.com>
2021-04-15 10:30:44 +0545

    Lifted state from LedgerTab to SelectWalletModal

--- Frontend/src/Components/Header/SelectWalletModal/LedgerTab/index.js
+++ Frontend/src/Components/Header/SelectWalletModal/LedgerTab/index.js
@@ -1,104 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import Transport from '@ledgerhq/hw-transport-u2f';
-import AppIcx from '@ledgerhq/hw-app-icx';
-import { NotificationManager } from 'react-notifications';
+import React from 'react';
 import LedgerWalletTable from './LedgerWalletTable';
 import { Spinner } from 'react-bootstrap';
 
-const BASE_PATH = `44'/4801368'/0'/0'`;
 
 //------------------------------------------------------------------
 const LedgerTab = ({
     walletAddress,
     setWalletAddress,
     onClose,
-    callBackAfterSelectingWalletAddress
+    callBackAfterSelectingWalletAddress,
+    walletAddresses,
+    walletPaths,
+    currPage,
+    setCurrPage,
+    isConnecting,
 }) => {
-    const [isConnected, setIsConnected] = useState(false);
-    const [isConnecting, setIsConnecting] = useState(false);
-    const [icx, setIcx] = useState(null);
-    // const [walletAddresses, setWalletAddresses] = useState(['hx9038432859428523','hx9038432859428523','hx9038432859428523','hx9038432859428523', 'hx49854929ae83', 'hx328549854893594', 'hx894325982589']);
-    const [walletAddresses, setWalletAddresses] = useState([]);
-    const [walletPaths, setWalletPaths] = useState([]);
-    const [currPage, setCurrPage] = useState(1);
-    const addressPerPage = 4;
 
-    useEffect(() => {
-        connectToLedger();
-        // eslint-disable-next-line
-    }, [])
 
-    useEffect(() => {
-        if (isConnected) {
-            loadAddresses(false, icx);
-        }
-        // eslint-disable-next-line
-    }, [currPage]);
 
-    const loadAddresses = async (suppressError, icx) => {
-        let i = 0;
-        let counter;
-        const addresses = [];
-        const paths = [];
-        while (i < addressPerPage) {
-            suppressError = true;
-            counter = (currPage - 1) * addressPerPage + i;
-            console.log('counter = ', counter);
-            const path = `${BASE_PATH}/${counter}'`;
-            console.log('PATH= ', path);
-            let { address } = await icx.getAddress(path, false, true);
-            address = address.toString();
-            console.log('a wallet ', address);
-            addresses.push(address);
-            paths.push(path);
-            i++;
-        }
-        console.log('ADDRESSES FOR A PAGE: ', addresses);
-        setWalletAddresses(addresses);
-        setWalletPaths(paths);
-    };
 
-    const connectToLedger = () => {
-        let suppressError = false;
 
-        setIsConnecting(true);
-        Transport.create()
-            .then(async (transport) => {
-                transport.setDebugMode(false);
-                const icx = new AppIcx(transport);
-                setIcx(icx);
-                console.log('Transportation channel established');
-                try {
-                    setIsConnected(true);
-                    //test if its working first
-                    await icx.getAddress(`${BASE_PATH}/0'`, false, true);
-
-                    //our func here
-                    await loadAddresses(suppressError, icx);
-
-                    setIsConnecting(false);
-                } catch (error) {
-                    if (suppressError) {
-                        console.warn('Failed connecting to Ledger.', error.message);
-                        // NotificationManager.error(error.message instanceof string , "Connecting to Ledger Failed")
-                        NotificationManager.error("Connecting to Ledger Failed")
-                    }
-                    else {
-                        // NotificationManager.error(error, "Connecting to Ledger Failed")
-                        NotificationManager.error("Connecting to Ledger Failed")
-                        console.error(error);
-                    }
-                    setIsConnected(false);
-                    setIsConnecting(false);
-                }
-            })
-            .catch((error) => {
-                NotificationManager.error(error?.message, "Transport channel could not be established")
-                setIsConnecting(false);
-                setIsConnected(false);
-                onClose();
-            });
-    };
 
     return (
         <div className="ledger-tab">
--- Frontend/src/Components/Header/SelectWalletModal/index.js
+++ Frontend/src/Components/Header/SelectWalletModal/index.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Transport from '@ledgerhq/hw-transport-u2f';
+import AppIcx from '@ledgerhq/hw-app-icx';
+import { NotificationManager } from 'react-notifications';
 import { Modal, Row, Col, Button } from 'react-bootstrap';
 import ledgerImg from 'Assets/images/ledgern.png';
 import iconImg from 'Assets/images/iconn.png';
@@ -11,9 +14,26 @@ const selectWalletStyle = {
     paddingBottom: '10px'
 };
 
+const BASE_PATH = `44'/4801368'/0'/0'`;
+
 function SelectWalletModal({ walletAddress, setWalletAddress, callBackAfterSelectingWalletAddress, ...props }) {
 
     const [selectedWalletType, setSelectedWalletType] = useState("");
+    const [isConnected, setIsConnected] = useState(false);
+    const [isConnecting, setIsConnecting] = useState(false);
+    const [icx, setIcx] = useState(null);
+    // const [walletAddresses, setWalletAddresses] = useState(['hx9038432859428523','hx9038432859428523','hx9038432859428523','hx9038432859428523', 'hx49854929ae83', 'hx328549854893594', 'hx894325982589']);
+    const [walletAddresses, setWalletAddresses] = useState([]);
+    const [walletPaths, setWalletPaths] = useState([]);
+    const [currPage, setCurrPage] = useState(1);
+    const addressPerPage = 4;
+
+    useEffect(() => {
+        if (isConnected) {
+            loadAddresses(false, icx);
+        }
+        // eslint-disable-next-line
+    }, [currPage]);
 
     const handleSelectWalletType = async (walletType) => {
         setSelectedWalletType(walletType);
@@ -29,9 +49,74 @@ function SelectWalletModal({ walletAddress, setWalletAddress, callBackAfterSelec
             props.onHide();
 
         } else if (walletType === WALLET_TYPE.LEDGER) {
-
+            connectToLedger();
         }
     }
+
+    const loadAddresses = async (suppressError, icx) => {
+        let i = 0;
+        let counter;
+        const addresses = [];
+        const paths = [];
+        while (i < addressPerPage) {
+            suppressError = true;
+            counter = (currPage - 1) * addressPerPage + i;
+            console.log('counter = ', counter);
+            const path = `${BASE_PATH}/${counter}'`;
+            console.log('PATH= ', path);
+            let { address } = await icx.getAddress(path, false, true);
+            address = address.toString();
+            console.log('a wallet ', address);
+            addresses.push(address);
+            paths.push(path);
+            i++;
+        }
+        console.log('ADDRESSES FOR A PAGE: ', addresses);
+        setWalletAddresses(addresses);
+        setWalletPaths(paths);
+    };
+
+    const connectToLedger = () => {
+        let suppressError = false;
+
+        setIsConnecting(true);
+        Transport.create()
+            .then(async (transport) => {
+                transport.setDebugMode(false);
+                const icx = new AppIcx(transport);
+                setIcx(icx);
+                console.log('Transportation channel established');
+                try {
+                    setIsConnected(true);
+                    //test if its working first
+                    await icx.getAddress(`${BASE_PATH}/0'`, false, true);
+
+                    //our func here
+                    await loadAddresses(suppressError, icx);
+
+                    setIsConnecting(false);
+                } catch (error) {
+                    if (suppressError) {
+                        console.warn('Failed connecting to Ledger.', error.message);
+                        // NotificationManager.error(error.message instanceof string , "Connecting to Ledger Failed")
+                        NotificationManager.error("Connecting to Ledger Failed")
+                    }
+                    else {
+                        // NotificationManager.error(error, "Connecting to Ledger Failed")
+                        NotificationManager.error("Connecting to Ledger Failed")
+                        console.error(error);
+                    }
+                    setIsConnected(false);
+                    setIsConnecting(false);
+                }
+            })
+            .catch((error) => {
+                NotificationManager.error(error?.message, "Transport channel could not be established")
+                setIsConnecting(false);
+                setIsConnected(false);
+                props.onHide();
+            });
+    };
 
     return (
         <Modal
@@ -74,6 +159,11 @@ function SelectWalletModal({ walletAddress, setWalletAddress, callBackAfterSelec
                                 onClose={() => props.onHide()}
                                 walletAddress={walletAddress}
                                 setWalletAddress={setWalletAddress}
+                                walletAddresses={walletAddresses}
+                                walletPaths={walletPaths}
+                                currPage={currPage}
+                                setCurrPage={setCurrPage}
+                                isConnecting={isConnecting}
                                 callBackAfterSelectingWalletAddress={() => {
                                     if (callBackAfterSelectingWalletAddress && callBackAfterSelectingWalletAddress instanceof Function) {
                                         callBackAfterSelectingWalletAddress();
