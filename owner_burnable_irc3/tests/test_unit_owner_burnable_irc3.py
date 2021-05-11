10f08b95d87166fabd94a60dd95c30ba2eb864e1
lilixac <hinewton25@gmail.com>
2021-05-11 16:50:16 +0545

    Owner Burnable Token Complete

---
+++ owner_burnable_irc3/tests/test_unit_owner_burnable_irc3.py
@@ -0,0 +1,12 @@
+# from ..owner_burnable_irc3 import OwnerBurnableIRC3
+# from tbears.libs.scoretest.score_test_case import ScoreTestCase
+
+
+# class TestOwnerBurnableIRC3(ScoreTestCase):
+
+#     def setUp(self):
+#         super().setUp()
+#         self.score = self.get_score_instance(OwnerBurnableIRC3, self.test_account1)
+
+#     def test_hello(self):
+#         self.assertEqual(self.score.hello(), "Hello")
