e35e7e89c42d94977170aa3c58e5d2798877cb4d
lilixac <hinewton25@gmail.com>
2021-06-04 18:01:58 +0545

    Script to generate zip files

--- zipscript.sh
+++ zipscript.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# IRC2 Zips
 zip -r basic_irc2.zip basic_irc2 -x "basic_irc2/tests/*"
 zip -r mintable_irc2.zip mintable_irc2 -x "mintable_irc2/tests/*"
 zip -r burnable_irc2.zip burnable_irc2 -x "burnable_irc2/tests/*"
@@ -9,3 +10,9 @@ zip -r mintpause_irc2.zip mintpause_irc2 -x "mintpause_irc2/tests/*"
 zip -r mintburn_irc2.zip mintburn_irc2 -x "mintburn_irc2/tests/*"
 zip -r mintburnpause_irc2.zip mintburnpause_irc2 -x "mintburnpause_irc2/tests/*"
 zip -r complete_irc2.zip complete_irc2 -x "complete_irc2/tests/*"
+
+# IRC3 Zips
+zip -r mintable_irc3.zip mintable_irc3 -x "mintable_irc3/tests/*"
+zip -r burnable_irc3.zip burnable_irc3 -x "burnable_irc3/tests/*"
+zip -r owner_mintable_irc3.zip owner_mintable_irc3 -x "owner_mintable_irc3/tests/*"
+zip -r owner_burnable_irc3.zip owner_burnable_irc3 -x "owner_burnable_irc3/tests/*"
